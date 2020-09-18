b39f99fddb082be5835a64c9e6eca932e8f0ba70
Carl J <carlj1881@yahoo.com>
2020-09-18 02:21:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,7 +35,7 @@ shipping_manifest["musket" => 2]
 
 
   # add 4 gun powder to the shipping_manifest hash below
-shipping_manifest["gun powder"] = "4"
+shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
